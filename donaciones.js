1ddda0b596229f6d5c6ffa59963c36bef3b53bff
Brayan <46829069+brayandeaviila@users.noreply.github.com>
2024-11-20 13:37:16 -0500

    arregle la vaina de isa

--- donaciones.js
+++ donaciones.js
@@ -5,20 +5,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const anonimoSection = document.getElementById("anonimo-section");
 
     tipoDonante.addEventListener("change", (e) => {
-        
         personaSection.classList.add("hidden");
+        personaSection.classList.remove("form");
         empresaSection.classList.add("hidden");
+        empresaSection.classList.remove("form");
         anonimoSection.classList.add("hidden");
+        anonimoSection.classList.remove("form");
+
         const selectedValue = e.target.value;
         let targetSection = null;
         if (selectedValue === "persona") {
             personaSection.classList.remove("hidden");
+            personaSection.classList.add("form");
             targetSection = personaSection;
         } else if (selectedValue === "empresa") {
             empresaSection.classList.remove("hidden");
+            empresaSection.classList.add("form");
             targetSection = empresaSection;
         } else if (selectedValue === "anonimo") {
             anonimoSection.classList.remove("hidden");
+            anonimoSection.classList.add("form");
             targetSection = anonimoSection;
         }
         if (targetSection) {
